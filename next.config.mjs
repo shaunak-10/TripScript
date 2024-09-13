82e592ac4b2268b6ba4bc305a3689315dffede81
shaunak-10 <shaunakmehta1012@gmail.com>
2024-09-13 14:38:52 +0530

    nothing

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['source.unsplash.com', 'images.unsplash.com'],
-    }
+  images: {
+    domains: ["source.unsplash.com", "images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
